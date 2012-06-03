1ed954a8f0fb285b693bfd03d9514e40c4c81674
Andrius Bentkus <andrius.bentkus@gmail.com>
2012-06-03 18:47:58 +0200

    Remove comments.

--- u5/aufgabe5.3.sql
+++ u5/aufgabe5.3.sql
@@ -1,17 +1,19 @@
 
-/*
+-- a.)
+
 SELECT konto.name, COUNT(*) FROM kunde, konto, ueberweisung as u
 WHERE kunde.blz = konto.blz AND kunde.kundennr = konto.kundennr AND kunde.blz = u.blz AND konto.kontonr = u.kontonr
 GROUP BY konto.name, kunde.kundennr
 -- brauchen wir nicht, da mindenstens eins mit der existenz gleich steht
 -- HAVING COUNT(*) > 0
-*/
 
-/*
+
+-- b.)
+
 SELECT k.kontonr, MAX(u.betrag) FROM konto as k, ueberweisung as u
 WHERE k.blz = u.blz AND k.kontonr = u.kontonr
 GROUP BY k.kontonr
-*/
+
 
 -- c.)
 
@@ -22,7 +24,5 @@ ORDER BY count LIMIT 1;
 
 -- d.)
 
-/*
 SELECT * FROM kunde
 WHERE kunde.kundennr NOT IN (SELECT kundennr FROM konto);
-*/
